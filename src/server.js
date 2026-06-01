beb8388a450baf568df2aa695f0f31d80a078072
alina <alina1998hryhorenko@gmail.com>
2026-06-01 11:44:57 +0200

    Fix homework requirements

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
 
-const express = require('express');
-const cors = require('cors');
-const pino = require('pino-http');
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(pinoHttp());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
