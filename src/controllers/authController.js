3ab7634960c0198a2bf4a05b4e43c68754f8c053
alina <alina1998hryhorenko@gmail.com>
2026-06-02 15:31:48 +0200

    fix auth controller exports and expired session cleanup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,63 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none',
+};
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(201).json(user);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(200).json(user);
+};
+
 export const refreshUserSession = async (req, res) => {
   const { sessionId, refreshToken } = req.cookies;
 
@@ -29,4 +89,18 @@ export const refreshUserSession = async (req, res) => {
   res.status(200).json({
     message: 'Session refreshed',
   });
+};
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId', cookieOptions);
+  res.clearCookie('accessToken', cookieOptions);
+  res.clearCookie('refreshToken', cookieOptions);
+
+  res.status(204).send();
 };
