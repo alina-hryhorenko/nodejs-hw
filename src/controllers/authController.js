dcd48c8e540ecd3824aeaa85d165d76f2e8340a2
alina <alina1998hryhorenko@gmail.com>
2026-06-16 18:51:23 +0200

    fix sendMail filename

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 const cookieOptions = {
   httpOnly: true,
