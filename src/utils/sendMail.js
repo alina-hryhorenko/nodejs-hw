f425605fc27ae2004b7250dfb610c74128192c49
alina <alina1998hryhorenko@gmail.com>
2026-06-16 16:07:17 +0200

    debug smtp env

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
 export const sendMail = async (options) => {
+  console.log('SMTP_USER:', process.env.SMTP_USER);
+  console.log('SMTP_PASSWORD:', process.env.SMTP_PASSWORD);
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
